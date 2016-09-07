bcc791dc7545db9c9f10d7ac8fe7a8f110c78f24
InamKhan94 <ullahkhaninam@gmail.com>
2016-09-07 22:14:39 +0500

    1st comit

---
+++ www/js/notestore.js
@@ -0,0 +1,57 @@
+angular.module('noteStore',[])
+ .factory('NoteStore',function(){
+  
+  var notes=angular.fromJson(window.localStorage['note'] || '[]');
+  
+  function persist(){
+    window.localStorage['note']=angular.toJson(notes);
+  }
+  return {
+    list: function(){
+      return notes;
+    },
+    
+    get: function(noteID){
+        for(var i=0; i<notes.length; i++)
+        {
+          if(notes[i].id === noteID){
+            return notes[i];
+          }
+        }
+        return undefined;
+      },
+     
+    create:function(note){
+        notes.push(note);
+        persist();
+    },
+
+    update: function(note){
+       for(var i=0; i<notes.length; i++)
+        {
+          if(notes[i].id === note.id){
+            notes[i]=note;
+            persist();
+            return;
+          }
+        }
+    }, 
+
+    move: function(note,fromIndex,toIndex){
+      notes.splice(fromIndex,1);
+      notes.splice(toIndex,0,note);
+      persist();
+    },
+    
+    remove: function(noteId){
+       for(var i=0; i<notes.length; i++)
+        {
+          if(notes[i].id === noteId){
+            notes.splice(i,1);
+            persist();
+            return;
+          }
+        }
+    }
+  };
+});
